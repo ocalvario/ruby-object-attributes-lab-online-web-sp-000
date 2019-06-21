5f3e3df5089ccff01d7660e72d7d52190dc0761f
Oswald Calvario <oswald.calvario@gmail.com>
2019-06-21 18:12:19 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,19 @@
 class Dog 
-
-def initialize(name)
-    @name = name
-  end
  
   def name
     @name
   end
  
-  def name=(new_name)
-    @name = new_name
+  def name=(name)
+    @name = name
   end
-  
+
+ def breed
+    @breed
+  end
+ 
+  def breed=(breed)
+    @breed = breed
+  end
+
+end
