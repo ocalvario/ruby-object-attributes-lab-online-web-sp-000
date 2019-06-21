ba0121e03bd6dbdd89b1987492c067fc7208d85f
Oswald Calvario <oswald.calvario@gmail.com>
2019-06-21 17:59:46 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,11 @@
 class Dog 
-
-def initialize(name)
-    @name = name
-  end
  
   def name
     @name
   end
  
-  def name=(new_name)
-    @name = new_name
+  def name=(name)
+    @name = name
   end
-  
+
+end
